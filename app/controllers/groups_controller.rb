ae9ff067b13a3c6c5f2504666a9d0671ca9c44ff
lance <xiakailun919@163.com>
2017-04-03 09:18:39 +0800

    fix bug

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
-  before_action :authenticate_user! , only: [:update, :destroy, :edit]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
     @groups = Group.all
@@ -38,7 +38,7 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-  
+
     @group.destroy
     flash[:alert] = "Group deleted"
     redirect_to groups_path
@@ -54,6 +54,7 @@ class GroupsController < ApplicationController
       redirect_to root_path, alert: "You have no permission."
     end
   end
+
 
   def group_params
     params.require(:group).permit(:title, :description)
